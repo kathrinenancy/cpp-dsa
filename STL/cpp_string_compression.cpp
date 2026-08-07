f969ea69fdc689117097c856ef69d4ee8f2159b2
Katharine Nancy N <nancy25019@gmail.com>
2026-08-07 22:01:35 +0530

    Add problem description for string compression
    Added problem description for string compression task.

--- STL/cpp_string_compression.cpp
+++ STL/cpp_string_compression.cpp
@@ -1,3 +1,19 @@
+/*
+Problem Description
+
+Bob is building a chat app that stores text messages efficiently.
+
+To save space, he wants to compress repeated characters in a message.
+
+    The compression rule is consecutive repeated characters are replaced by character + count
+
+    The count is always included, even if it's 1
+
+Can you help Bob to find the compressed string as he is busy.
+
+    If the compressed string length is greater than or equal to the original string, return the original string instead.*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
