9916326abd68ba58b3eac8997302e6b05ea446b5
Katharine Nancy N <nancy25019@gmail.com>
2026-08-07 22:41:31 +0530

    Add problem statement for Polycrap Loves Music
    Added a problem statement and description for the music listening challenge.

--- STL/favorite_artist_listening_time.cpp
+++ STL/favorite_artist_listening_time.cpp
@@ -1,3 +1,28 @@
+/*
+Problem: Polycrap Loves Music
+
+Description:
+After a long day of coding challenges, Polycrap decides to relax by
+listening to music. However, Polycrap only listens to songs that satisfy
+both of the following conditions:
+
+1. The song duration must be greater than or equal to K minutes.
+2. The song must be performed by one of Polycrap's favorite artists.
+
+You are given:
+- K: The minimum duration required for a song.
+- favoriteArtists: A list of M favorite artists.
+- songs: A list of N songs, where each song contains:
+    - duration: The length of the song in minutes.
+    - artistName: The artist who performed the song.
+
+Return the total number of minutes Polycrap spends listening to songs
+that satisfy both conditions.
+
+Topic: C++ STL
+Concepts: vector, pair, string.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
