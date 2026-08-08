0ddfb18d32f3462c699a95ef1104109158a364ad
Katharine Nancy N <nancy25019@gmail.com>
2026-08-08 11:07:54 +0530

    Add problem statement for keyboard typing distance
    Added problem description and details for calculating keyboard typing distance.

--- Math-Basics/keyboard_typing_distance.cpp
+++ Math-Basics/keyboard_typing_distance.cpp
@@ -1,3 +1,29 @@
+/*
+Problem: Keyboard Typing Distance
+
+Description:
+You are given a custom keyboard layout arranged in a grid. Each key has
+a row and column position.
+
+The task is to calculate the total distance travelled by a single finger
+while typing a given string.
+
+The finger starts at the first key of the string and moves from one key
+to the next.
+
+For two keys located at (r1, c1) and (r2, c2), the distance between them
+is calculated using:
+
+    |r1 - r2| + |c1 - c2|
+
+The total distance is the sum of the distances travelled between all
+consecutive characters while typing the given string.
+
+Topic: Math Basics
+Concepts: Coordinates, Absolute Value, Grid, Distance
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
