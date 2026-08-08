3676a03ce32c0452c8cf12e7703a49e7bbbc3bc4
Katharine Nancy N <nancy25019@gmail.com>
2026-08-08 11:21:55 +0530

    Update song_duration_partition.cpp

--- Math-Basics/song_duration_partition.cpp
+++ Math-Basics/song_duration_partition.cpp
@@ -1,3 +1,31 @@
+/*
+Problem: Luntik Loves Singing
+
+Description:
+Luntik has a collection of songs with different durations:
+
+- a one-minute songs
+- b two-minute songs
+- c three-minute songs
+
+He wants to divide all the songs into two concerts such that every song
+is included in exactly one of the two concerts.
+
+The duration of a concert is the sum of the durations of all songs
+assigned to it.
+
+The goal is to minimize the absolute difference between the total
+durations of the two concerts.
+
+Task:
+Determine the minimum possible difference, in minutes, between the
+durations of the two concerts.
+
+Topic: Math Basics
+Concepts: Arithmetic.
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
